ac22b6deac5a045a1ad93d5f3b3a54e10cdd1211
mickey <mickeyjiyestha@gmail.com>
2024-12-15 16:17:33 +0800

    final comit

--- src/store/favorite.js
+++ src/store/favorite.js
@@ -1,15 +1,13 @@
-// favorite.js
 import { ref } from "vue";
 
 const favoriteProducts = ref([]);
 
-// Fungsi untuk menambahkan atau menghapus favorit
 const toggleFavorite = (product) => {
   const index = favoriteProducts.value.findIndex((p) => p.id === product.id);
   if (index === -1) {
-    favoriteProducts.value.push(product); // Tambahkan jika belum ada
+    favoriteProducts.value.push(product);
   } else {
-    favoriteProducts.value.splice(index, 1); // Hapus jika sudah ada
+    favoriteProducts.value.splice(index, 1);
   }
 };
 
